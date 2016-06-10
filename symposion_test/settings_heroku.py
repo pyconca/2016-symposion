63b3dc8cb5685b22763a7077af83e11b53ff3583
Terry Yanchynskyy <terry.yanchynskyy@stjoseph.com>
2016-06-09 20:11:43 -0400

    serve static

--- symposion_test/settings_heroku.py
+++ symposion_test/settings_heroku.py
@@ -3,3 +3,14 @@ from .settings import *
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# STATIC
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
