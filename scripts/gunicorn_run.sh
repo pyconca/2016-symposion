37ba56ba8d097f98d497d42d373af1882449e228
Terry Yanchynskyy <tarasics@gmail.com>
2016-07-07 19:55:35 -0400

    gunicorn entry script and supervisor config; `setproctitle` to see process names

---
+++ scripts/gunicorn_run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+NAME="symposion2016"
+DJANGODIR=/srv/www/pycon.ca/staging.2016/django/symposion2016
+SOCKFILE=/srv/www/pycon.ca/staging.2016/django/run/gunicorn.sock
+USER=deploy
+GROUP=deploy
+NUM_WORKERS=3
+DJANGO_SETTINGS_MODULE=symposion2016.settings.prod
+DJANGO_WSGI_MODULE=symposion2016.wsgi
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ../env/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
