aefcecaf0a68afc8b71d2c06ac4cb7258ef9dacc
Terry Yanchynskyy <tarasics@gmail.com>
2016-07-07 17:02:24 -0400

    refactoring settings

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion_test.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion_test.settings.dev")
 
     from django.core.management import execute_from_command_line
 
