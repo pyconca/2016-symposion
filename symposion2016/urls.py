b8baef83484a5aaa8839ec4926ea82dee1193a0a
Terry Yanchynskyy <tarasics@gmail.com>
2016-07-08 00:08:59 -0400

    README: quickstart and deployment instructions

--- symposion2016/urls.py
+++ symposion2016/urls.py
@@ -33,4 +33,5 @@ urlpatterns = patterns(
     # url(r"^", include("symposion.cms.urls")),
 )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
