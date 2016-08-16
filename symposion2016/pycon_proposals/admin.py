7fd3ab348b07f71dc327f4f6e489a9ccaa20706a
Terry Yanchynskyy <tarasics@gmail.com>
2016-08-16 19:56:44 -0400

    proposal admin list fields and filters

--- symposion2016/pycon_proposals/admin.py
+++ symposion2016/pycon_proposals/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import TalkProposal, TutorialProposal
 
 
-admin.site.register(TalkProposal)
-admin.site.register(TutorialProposal)
+@admin.register(TalkProposal, TutorialProposal)
+class TalkProposalAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'kind', 'tutorial', 'audience_level', 'speaker')
+    list_filter = ('kind', 'recurring', 'tutorial', 'audience_level', 'speaker', 'duration')
+    search_fields = ('title', 'description', 'abstract')
