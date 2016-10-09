9f3dca005073693a6a76f6dc21709da91b651b9c
onebit0fme <tarasics@gmail.com>
2016-10-08 20:08:52 -0400

    added speaker email and username to api response

--- symposion2016/conf_api/serializers.py
+++ symposion2016/conf_api/serializers.py
@@ -20,6 +20,8 @@ class ConferenceSerializer(serializers.ModelSerializer):
 
 
 class SpeakerSerialize(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.CharField(source='user.email', read_only=True)
 
     class Meta:
         model = Speaker
